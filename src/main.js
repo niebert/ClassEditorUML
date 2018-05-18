a66557ec6a28aee4e58f062231c4f5beefbb5402
Engelbert Niehaus <niehaus@uni-landau.de>
2018-05-18 16:11:42 +0200

    saveJSON reference to vJSON.data.... removed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /* ---------------------------------------
  Exported Module Variable: JSONEditor4Code
  Package:  jsoneditor4code
- Version:  0.9.1  Date: 2018/05/18 16:01:53
+ Version:  0.9.1  Date: 2018/05/18 16:11:12
  Homepage: https://niebert.github.io/ClassEditorUML
  Author:   Engelbert Niehaus
  License:  MIT
