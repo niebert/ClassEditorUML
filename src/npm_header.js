a8244ea9e6c411ad4469fae20687c2f4796245fa
Engelbert Niehaus <niehaus@uni-landau.de>
2018-05-18 16:20:20 +0200

    filesaver.js missing in build.js

--- src/npm_header.js
+++ src/npm_header.js
@@ -1,7 +1,7 @@
 /* ---------------------------------------
  Exported Module Variable: JSONEditor4Code
  Package:  jsoneditor4code
- Version:  0.9.1  Date: 2018/05/18 16:11:12
+ Version:  0.9.1  Date: 2018/05/18 16:20:01
  Homepage: https://niebert.github.io/ClassEditorUML
  Author:   Engelbert Niehaus
  License:  MIT
