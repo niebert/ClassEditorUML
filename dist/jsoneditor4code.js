a35a5631094528415e49b2e33bc23a789876d16b
Engelbert Niehaus <niehaus@uni-landau.de>
2020-10-07 17:30:41 +0200

    Update Handlebars4Code to Version 1.2.37

--- dist/jsoneditor4code.js
+++ dist/jsoneditor4code.js
@@ -1,7 +1,7 @@
 /* ---------------------------------------
  Exported Module Variable: JSONEditor4Code
  Package:  jsoneditor4code
- Version:  0.9.13  Date: 2020/10/07 15:24:54
+ Version:  0.9.13  Date: 2020/10/07 16:51:22
  Homepage: https://niebert.github.io/ClassEditorUML
  Author:   Engelbert Niehaus
  License:  MIT
--- docs/js/jsoneditor4code.js
+++ docs/js/jsoneditor4code.js
@@ -3135,7 +3135,7 @@ exports.SourceNode = require('./lib/source-node').SourceNode;
 /* ---------------------------------------
  Exported Module Variable: JSONEditor4Code
  Package:  jsoneditor4code
- Version:  0.9.13  Date: 2020/10/07 15:24:54
+ Version:  0.9.13  Date: 2020/10/07 16:51:22
  Homepage: https://niebert.github.io/ClassEditorUML
  Author:   Engelbert Niehaus
  License:  MIT
